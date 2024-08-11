af133479a296b1b5d6f3dcbdb8c53d35be851b26
Daniel Bedggood <ddbedggood@gmail.com>
2024-08-11 18:03:49 +1200

    Create button on each listing that reads listing id values

--- content.js
+++ content.js
@@ -7,12 +7,28 @@ const updateTgColElements = () => {
 
   for (let i = 0; i < listings.length; i++) {
     const listing = listings[i];
+    const listingId = listing.firstElementChild
+      .getAttribute("data-aria-id")
+      .split("-")[1];
+
     const addressSubtitle = listing.querySelector(
       "tm-property-search-card-address-subtitle"
     );
-    if (addressSubtitle) {
-      addressSubtitle.style.color = "blue";
-    }
+    addressSubtitle.style.color = "blue";
+
+    const listedDateContainer = listing.getElementsByClassName(
+      "tm-property-premium-listing-card__pre-title-container"
+    )[0];
+
+    const button = document.createElement("button");
+    button.textContent = "Hide Listing";
+    button.addEventListener("click", (e) => {
+      e.preventDefault();
+      e.stopPropagation();
+      console.log("BLOCKED", listingId);
+    });
+
+    listedDateContainer.appendChild(button);
   }
 };
 
