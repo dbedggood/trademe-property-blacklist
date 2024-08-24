b7b14df9f7591293bd650b94eceaebca89cd9b90
Daniel Bedggood <ddbedggood@gmail.com>
2024-08-24 20:22:29 +1200

    Add localStorage calls

--- content.js
+++ content.js
@@ -1,5 +1,7 @@
 console.log("Content script loaded");
 
+const blacklist = JSON.parse(localStorage.getItem("blacklist") ?? "[]");
+
 const updateTgColElements = () => {
   const listings = document.getElementsByTagName(
     "tm-property-premium-listing-card"
@@ -7,6 +9,7 @@ const updateTgColElements = () => {
 
   for (let i = 0; i < listings.length; i++) {
     const listing = listings[i];
+
     const listingId = listing.firstElementChild
       .getAttribute("data-aria-id")
       .split("-")[1];
@@ -20,12 +23,28 @@ const updateTgColElements = () => {
       "tm-property-premium-listing-card__pre-title-container"
     )[0];
 
+    const isListingBlocked = blacklist?.includes(listingId);
+
+    if (isListingBlocked) {
+      listing.style.filter = "grayscale(1)";
+    }
+
     const button = document.createElement("button");
     button.textContent = "Hide Listing";
     button.addEventListener("click", (e) => {
       e.preventDefault();
       e.stopPropagation();
+
+      if (isListingBlocked) {
+        console.log("ALREADY BLOCKED", listingId);
+        return;
+      }
       console.log("BLOCKED", listingId);
+      listing.style.filter = "grayscale(1)";
+      localStorage.setItem(
+        "blacklist",
+        JSON.stringify([...blacklist, listingId])
+      );
     });
 
     listedDateContainer.appendChild(button);
